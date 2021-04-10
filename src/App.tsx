1c4fc25592cefa5383c099c01bbeaeb3a22f7f5c
RodyGL <rody.guzman@outlook.com>
2021-04-09 22:18:37 -0600

    Add code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AppProviders } from './AppProviders';
 import { MainNavbar } from './features/main-navbar/MainNavbar';
-import PageOneIndex from './pages/page-one/PageOneIndex';
-import PageTwoIndex from './pages/page-two/PageTwoIndex';
+
+const PageOneIndex = React.lazy(() => import('./pages/page-one/PageOneIndex'));
+const PageTwoIndex = React.lazy(() => import('./pages/page-two/PageTwoIndex'));
 
 export function App() {
   return (
-    <div className="flex flex-col w-full h-screen bg-ice-blue text-black">
-      <AppProviders>
-        <MainNavbar />
+    <Suspense fallback={null}>
+      <div className="flex flex-col w-full h-screen bg-ice-blue text-black">
+        <AppProviders>
+          <MainNavbar />
 
-        <Routes>
-          <Route path="page-1" element={<PageOneIndex />} />
-          <Route path="page-2" element={<PageTwoIndex />} />
+          <Routes>
+            <Route path="page-1" element={<PageOneIndex />} />
+            <Route path="page-2" element={<PageTwoIndex />} />
 
-          <Route path="*" element={<Navigate replace to="/page-1" />} />
-        </Routes>
-      </AppProviders>
-    </div>
+            <Route path="*" element={<Navigate replace to="/page-1" />} />
+          </Routes>
+        </AppProviders>
+      </div>
+    </Suspense>
   );
 }
